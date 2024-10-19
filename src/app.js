79edfca708335804bbd191eb0435b45055fe7b7c
Hazem Alrfati <90184855+Alrfati-Hazem@users.noreply.github.com>
2024-10-19 15:25:32 +0300

    Init project setup (#3)
    * test deploy
    
    * test
    
    * test postgres
    
    * init project

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const { sequelize } = require('./db/models');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    status: 'suceess',
+    data: 'welcome',
+  });
+});
+
+app.use('*', (req, res) => {
+  res.status(404).json({ status: 'fail', message: 'Not Found' });
+});
+
+app.listen(process.env.APP_PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+});
